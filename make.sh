90a2d9e3fff5b82cc95a1a85ccb3d25a97b5f250
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-03-31 09:35:12 +0200

    new example values

--- make.sh
+++ make.sh
@@ -11,6 +11,7 @@ function site-values() {
 
 function create-secrets(){
     echo
+# TODO: adapt for current version
 #    kubectl create secret generic db-user-pass  --from-file=./private/password.txt
 #    kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
 #    kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
@@ -19,7 +20,7 @@ function create-secrets(){
 
 function upgrade-dev() {
     set -x
-    helm upgrade -i -n ${NAMESPACE:?} oda . \
+    helm upgrade -i -n ${ODA_NAMESPACE:?} oda . \
         -f $(site-values) \
         --set dqueue.image.tag="$(cd charts/dqueue-chart/dqueue; git describe --always)" \
         --set dda.image.tag="$(cd charts/dda-chart/dda; git describe --always)" \
